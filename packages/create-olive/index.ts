a047e806b3c8948b5955d32a860fbf6a788c7f7a
Anthony Ferraro <tony@Anthonys-MBP.lan>
2023-11-04 01:13:08 -0400

    add template package

--- packages/create-olive/index.ts
+++ packages/create-olive/index.ts
@@ -1,2 +1,28 @@
 #!/usr/bin/env bun
-console.log("create 🫒")
+
+import arg from "arg"
+import prompts from "prompts"
+
+// @ts-ignore
+import { version } from "./package.json"
+
+// console.log("create 🫒")
+
+const args = arg({
+	"--help": Boolean,
+	"--version": Boolean,
+    "--name": String,
+
+	"-h": "--help",
+	"-v": "--version",
+    "-n": "--name"
+})
+
+const res = await prompts({
+    type: "text",
+    name: "appName",
+    message: "What is the name of your app?",
+    initial: "olive",
+})
+
+console.log(res)
