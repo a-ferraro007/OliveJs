b916f9f14bf0f7570ac786b6dfe3d86eff230467
Anthony Ferraro <tony@Anthonys-MBP.lan>
2023-11-03 22:57:43 -0400

    create template

---
+++ packages/create-olive/template/src/components/Counter.tsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react"
+
+const Counter = () => {
+    const [count, setCount] = useState(0)
+    useEffect(( ) => {
+        const interval = setInterval(() => {
+            setCount((count) => count + 1)   
+        }, 1000)
+  
+        return () => {       
+            clearInterval(interval)
+        }
+    }) 
+    return ( 
+        <div>  {count}</div> 
+    )   
+} 
+         
+export default Counter    
