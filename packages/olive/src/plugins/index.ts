39f3104770b76f425b29e28fe03b81a7eb3803bd
Tony Ferraro <adferra24@gmail.com>
2024-09-10 16:23:30 -0400

    chore: delete old plugin files

--- packages/olive/src/plugins/index.ts
+++ packages/olive/src/plugins/index.ts
@@ -1,6 +1,5 @@
 import globalReplacePlugin from "./globalReplacePlugin";
 import indexHTMLPlugin from "./indexHTMLPlugin";
 import postCSSLoader from "./postCSSPlugin";
-import publicDirectoryPlugin from "./publicDirectoryPlugin";
 
-export { globalReplacePlugin, indexHTMLPlugin, postCSSLoader, publicDirectoryPlugin };
+export { globalReplacePlugin, indexHTMLPlugin, postCSSLoader };
